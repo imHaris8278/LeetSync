61d8e0ceb17097371941460899d7a33c27fcd224
Mirza Muhammad Haris Baig <harisbaig7081@gmail.com>
2024-12-18 12:10:38 +0500

    Time: 5 ms (23.28%) | Memory: 8.6 MB (50.29%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+
+        int original = x;
+        long reversed = 0;
+        
+        while (x > 0)
+        {
+            reversed = reversed * 10 + x % 10;;
+            x /= 10;
+        }
+
+        if (original == reversed)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
